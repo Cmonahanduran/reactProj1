c9c59e4ea1df7439eacefa6b2944d91fc3768999
Cmonahanduran <118871270+Cmonahanduran@users.noreply.github.com>
2023-03-18 15:00:55 -0700

    base app comp1

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          <ExpenseItem></ExpenseItem>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
